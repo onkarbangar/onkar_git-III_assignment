6306174fb832e878736f9f6dc3ad384ea03bec7e
rhutikthakare <thakarerhutik@gmail.com>
2022-12-07 12:50:50 +0530

    prime code fixed

--- Prime_Number.js
+++ Prime_Number.js
@@ -1,12 +1,25 @@
 function identifyPrime(num) {
      let x=0;
     for(let i=1; i<=num; i++){
-        if (num%i===0){
+        if (num%i==0){
             x++;
         } 
-        }if (x==0){
-            console.log("Yes");
+        if (x==2){
+            return true;
             }
-        else{console.log("No");
-            } 
+             return false;
+             
 } 
+
+let ans=identifyprime(13);
+
+if(ans==true){
+
+console.log("prime")
+
+}
+else{
+
+console.log("not a prime")
+
+}
